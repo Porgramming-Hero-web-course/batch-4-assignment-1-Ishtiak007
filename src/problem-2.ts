a999afae1f072f9457308f0915e6b7cb7f5888b8
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-11-07 14:01:24 +0600

    problem-3,8 files have been created. And in each file I have written the questions/requirements of its respective problems

--- src/problem-2.ts
+++ src/problem-2.ts
@@ -1,2 +1,4 @@
 {
+  // problem-2
+  // Create a TypeScript function removeDuplicates that accepts an array of numbers and returns a new array with duplicates removed. Preserve the original order of elements.
 }
---
+++ src/problem-3.ts
@@ -0,0 +1,4 @@
+{
+  // problem-3
+  // Write a TypeScript function countWordOccurrences that takes a sentence (string) and a word (string). It should return the number of times the word appears in the sentence, case-insensitively.
+}
